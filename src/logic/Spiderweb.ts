fc7a3a517bbc84d5b1b018f2d1a396d8083a3d86
KadinTucker <kadintucker@gmail.com>
2020-08-22 22:04:20 -0600

    Add docs to Spiderweb

--- src/logic/Spiderweb.ts
+++ src/logic/Spiderweb.ts
@@ -1,10 +1,17 @@
 /**
  * Represent a spider web
+ * Includes a list of nodes, of type SpiderwebNode,
+ * and an adjacency map, which gives the indices of
+ * each adjacent node for each node index
  */
 class Spiderweb {
 
-	nodes: Array<SpiderwebNode>;
+	nodes: Array<SpiderwebNode>; ///The list of nodes in the web, to be referenced by index
 
+	/**
+	 * Construct a Spiderweb
+	 * @param adjacencies the adjacency map, listing the adjacent indices for each node
+	 */
 	constructor(public adjacencies: Array<Array<Number>>) {
 		this.nodes = adjacencies.map(() => new SpiderwebNode());
 	}
@@ -13,6 +20,8 @@ class Spiderweb {
 
 /**
  * Represent a node of the spiderweb
+ * Contains a certain number of ants, 
+ * as well as any pheromones ants have placed
  */
 class SpiderwebNode {
 
@@ -22,17 +31,22 @@ class SpiderwebNode {
 } 
 
 /**
- * TODO: take in a parameter and validate the graph
+ * Validates a graph based on an adjacency map according the following rules:
+ * A graph is valid if: 
+ *  - All nodes have at least one adjacency (non-isolation)
+ *  - If a node is connected to another, that node shares the connection (symmetry)
+ *  - There exists a path of connections between any two nodes (connectivity)
+ * If any of these fail, returns false; otherwise true
  */
 function validateGraph(adjacencies: Array<Array<Number>>): boolean {
 	for (let i = 0; i < adjacencies.length; i++) {
-		// Existence of adjacencies
+		// Ensure non-isolation
 		if (adjacencies[i].length == 0) return false;
-		// Reflexivity
+		// Ensure symmetry
 		for (let j = 0; j < adjacencies[i].length; j++) {
 			if (!adjacencies[j].includes(i)) return false;
 		}
 	}
-	// TODO: check connectivity
+	// TODO: ensure connectivity
 	return true;
 }
