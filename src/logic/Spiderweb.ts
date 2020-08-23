606698ca0e72a69d06c13aecdcc5af854ec7a28d
EliaGorokhovsky <eliapgorokhovsky@gmail.com>
2020-08-22 21:43:45 -0600

    Add spiderweb boilerplate

--- src/logic/Spiderweb.ts
+++ src/logic/Spiderweb.ts
@@ -1,14 +1,38 @@
-
 /**
- * TODO: represent a spider web
+ * Represent a spider web
  */
 class Spiderweb {
+
+	nodes: Array<SpiderwebNode>;
+
+	constructor(public adjacencies: Array<Array<Number>>) {
+		this.nodes = adjacencies.map(() => new SpiderwebNode());
+	}
 
 }
 
 /**
+ * Represent a node of the spiderweb
+ */
+class SpiderwebNode {
+
+	ants = 0;
+	pheromones: Array<Number> = [];
+
+} 
+
+/**
  * TODO: take in a parameter and validate the graph
  */
-function validateGraph(): boolean {
-	return false;
+function validateGraph(adjacencies: Array<Array<Number>>): boolean {
+	for (let i = 0; i < adjacencies.length; i++) {
+		// Existence of adjacencies
+		if (adjacencies[i].length == 0) return false;
+		// Reflexivity
+		for (let j = 0; j < adjacencies[i].length; j++) {
+			if (!adjacencies[j].includes(i)) return false;
+		}
+	}
+	// TODO: check connectivity
+	return true;
 }
